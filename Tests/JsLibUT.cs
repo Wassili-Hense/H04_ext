ccadfb7975e5cb6d19d2a1aafe003713c2260a6d
Wassili Hense <hense.wassili@googlemail.com>
2017-10-17 17:10:05 +0200

    ADD Tests

---
+++ Tests/JsLibUT.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace X13 {
+  [TestClass]
+  public class JsLibUT {
+    [TestMethod]
+    public void TestMethod1() {
+    }
+  }
+}
